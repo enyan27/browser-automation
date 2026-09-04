4785088ccd6501b398e664d9b5391088d8658bbc
enyan27 <truongdat2325@gmail.com>
2026-09-04 15:24:57 +0700

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
+import { cn } from "@/lib/utils"
+import "./globals.css"
+
+const font = Inter({ subsets: ["latin"], variable: "--font-sans" })
+
+export const metadata: Metadata = {
+  title: { default: "♡", template: "%s ♡" }
+}
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "antialiased")}>
+        <ThemeProvider>
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
